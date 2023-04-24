014b1ef95de16bbcd3f54f81f5d40c3d3fbde9a9
xieshijie <xieshijie@kuaishou.com>
2023-04-24 19:47:47 +0800

    excel解析完成

--- src/excel.ts
+++ src/excel.ts
@@ -1,54 +1,87 @@
 import fs from 'fs';
 import ExcelJS, { Row } from 'exceljs';
+import axios from 'axios';
+import sharp from 'sharp';
+import imageSize from 'image-size';
+
 
 async function readExcel(data: Buffer): Promise<void> {
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(data);
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.load(data);
 
-  const worksheet = workbook.getWorksheet(1);
+    const worksheet = workbook.getWorksheet(1);
+    const outputDir = '/Users/xieshijie/Desktop/down';
 
-  worksheet.eachRow((row: Row, rowIndex: number) => {
-    console.log(`Row ${rowIndex}: ${JSON.stringify(row.values)}`);
-  });
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+    }
+
+    worksheet.eachRow(async (row: ExcelJS.Row, rowIndex: number) => {
+        const sendTime = row.getCell(1).text;
+        const sendText = row.getCell(2).text;
+        const imageUrl = row.getCell(3).text;
+
+        console.log(`Row ${rowIndex}: SendTime: ${sendTime}, SendText: ${sendText}`);
+
+        if (imageUrl) {
+            try {
+                const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+                const imageBuffer = Buffer.from(response.data);
+                const imagePath = `${outputDir}/image_${rowIndex}`;
+                const outputFilePath = `${imagePath}.jpg`; // Change the file extension as needed
+                await sharp(imageBuffer).toFile(outputFilePath);
+                console.log(`Image saved at ${outputFilePath}`);
+            } catch (error) {
+                console.error(`Error downloading image at row ${rowIndex}:`);
+            }
+        }
+    });
+}
+
+async function getImageBufferFromCell(workbook: ExcelJS.Workbook, cell: ExcelJS.Cell): Promise<Buffer | null> {
+    if (cell.type === ExcelJS.ValueType.Hyperlink) {
+        const imageId = (cell.value as ExcelJS.CellHyperlinkValue).hyperlink;
+        const media = workbook.model.media.find((m) => m.type === imageId);
+
+        if (media) {
+            return Buffer.from(media.buffer);
+        }
+    }
+
+    return null;
 }
 
 // 测试函数
 async function testReadExcel(filePath: string): Promise<void> {
-  fs.readFile(filePath, async (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-
-    await readExcel(data);
-  });
-}
-async function readFileExcel(file: File): Promise<void> {
-    const buffer = await new Promise<Buffer>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const { result } = event.target as FileReader;
-        if (result instanceof ArrayBuffer) {
-          resolve(Buffer.from(result));
-        } else {
-          reject(new Error('Unexpected result from FileReader'));
+    fs.readFile(filePath, async (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
         }
-      };
-      reader.onerror = (error) => {
-        reject(error);
-      };
-      reader.readAsArrayBuffer(file);
+
+        await readExcel(data);
     });
-  
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.load(buffer);
-  
-    const worksheet = workbook.getWorksheet(1);
-  
-    worksheet.eachRow((row: Row, rowIndex: number) => {
-      console.log(`Row ${rowIndex}: ${JSON.stringify(row.values)}`);
-    });
-  }
+}
+
+// 下载并读取 Excel 文件
+async function downloadAndReadExcel(url: string): Promise<void> {
+    try {
+        const response = await axios.get(url, {
+            responseType: 'arraybuffer',
+        });
+
+        const data = new Buffer(response.data);
+        await readExcel(data);
+    } catch (error) {
+        console.error('Error downloading file:', error);
+    }
+}
+
+
+
 
 // 运行测试函数
-// testReadExcel();
+// const filePath = '/Users/xieshijie/Desktop/down/test.xlsx';
+// testReadExcel(filePath);
+const url = 'https://coder-xieshijie-img-1253784930.cos.ap-beijing.myqcloud.com/wechat/test.xlsx';
+downloadAndReadExcel(url)
--- src/main.ts
+++ src/main.ts
@@ -14,16 +14,6 @@ import ExcelJS, { Row } from 'exceljs';
 
 const chatGPTBot = new ChatGPTBot();
 
-// 用于存储发送任务的数据结构
-interface Task {
-  sendTime: any;
-  sendText: any;
-  sendImagePath: any;
-}
-
-const tasks: Task[] = [];
-const savePath = '/Users/xieshijie/Desktop/down';
-
 const bot = WechatyBuilder.build({
   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
   puppet: "wechaty-puppet-wechat",
@@ -74,97 +64,31 @@ async function main() {
       console.log(`已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`);
       console.log(`已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`);
     })
-    // .on("message", async (message) => {
-    //   if (message.date().getTime() < initializedAt) {
-    //     return;
-    //   }
-    //   if (message.text().startsWith("/ping")) {
-    //     await message.say("pong");
-    //     return;
-    //   }
-    //   try {
-    //     await chatGPTBot.onMessage(message);
-    //   } catch (e) {
-    //     console.error(e);
-    //   }
-    // })
-    // .on('ready', async () => {
-    //   // 定义要发送的消息内容
-    //   const messageContent = '晚安，好梦！👀';
+    .on('ready', async () => {
+      // 定义要发送的消息内容
+      const messageContent = '晚安，好梦！👀';
 
-    //   // 定义要发送消息的联系人名称
-    //   const targetContactName = '谢世杰';
+      // 定义要发送消息的联系人名称
+      const targetContactName = '谢世杰';
 
-    //   // 创建定时任务，每天的 22:25 执行
-    //   schedule.scheduleJob('33 22 * * *', async () => {
-    //     const targetContact = await bot.Contact.find({ name: targetContactName });
+      // 创建定时任务，每天的 22:25 执行
+      schedule.scheduleJob('33 22 * * *', async () => {
+        const targetContact = await bot.Contact.find({ name: targetContactName });
 
-    //     if (targetContact) {
-    //       await targetContact.say(messageContent);
-    //       console.log(`定时消息已发送给 ${targetContactName}`);
-    //     } else {
-    //       console.log(`未找到联系人 ${targetContactName}`);
-    //     }
-    //   })
-    // })
-    // .on('message', async (message: Message) => {
-    //   const sender = message.talker();
-
-    //   if (sender && sender.name() === '谢世杰123' && message.type() === bot.Message.Type.Attachment) {
-    //     const attachment = await message.toFileBox();
-    //     console.log(`attachment name: ${attachment.name}`);
-
-    //     if (attachment.name.endsWith('.xlsx')) {
-    //       try {
-    //         const fileContent = await attachment.toBuffer;
-    //         const workbook = new Workbook();
-    //         // readExcel(fileContent);
-    //         // await workbook.xlsx.load(fileContent);
-    //         // const worksheet = workbook.getWorksheet(1);
-    //         // const tasks: Task[] = [];
-
-    //         // worksheet.eachRow((row, rowIndex) => {
-    //         //   if (rowIndex > 1) {
-    //         //     const sendTime = row.getCell(1).value;
-    //         //     const sendText = row.getCell(2).value;
-    //         //     const sendImagePath = row.getCell(3).value;
-
-    //         //     tasks.push({ sendTime, sendText, sendImagePath });
-    //         //   }
-    //         // });
-
-    //         // console.log("tasks info == " + JSON.stringify(tasks, null, 2));
-    //         // updateScheduledTasks(tasks);
-    //       } catch (error) {
-    //         console.error(`Error parsing Excel file: ${error}`);
-    //       }
-    //     }
-    //   }
-    // })
-    .on('message', async (message: Message) => {
-      // 检查消息类型是否为附件
-      if (message.type() === bot.Message.Type.Attachment) {
-        try {
-          // 获取 FileBox 对象
-          const fileBox = await message.toFileBox();
-    
-          // 获取文件名
-          const fileName = fileBox.name;
-    
-          // 设置保存文件的路径
-          const filePath = path.join(savePath, fileName);
-          console.log(`Saving file: ${fileName} (${fileBox.size} bytes)`);
-    
-          // 将文件保存到本地指定目录
-          await fileBox.toFile(filePath, true);
-    
-          console.log(`File saved to: ${filePath}`);
-        } catch (error) {
-          console.error(`Error saving file: ${error}`);
+        if (targetContact) {
+          await targetContact.say(messageContent);
+          console.log(`定时消息已发送给 ${targetContactName}`);
+        } else {
+          console.log(`未找到联系人 ${targetContactName}`);
         }
-      }
-    });
+      })
+    })
+    .on('message', async (message: Message) => {
+      const sender = message.talker();
 
+      
+    })
+    ;
 
   try {
     await bot.start();
@@ -175,60 +99,4 @@ async function main() {
   }
 }
 
-// 读取 Excel 文件
-async function readExcel(buffer: Buffer): Promise<void> {
-  const workbook = new ExcelJS.Workbook()
-  await workbook.xlsx.load(buffer)
-
-  const worksheet = workbook.getWorksheet(1)
-
-  worksheet.eachRow((row: Row, rowIndex: number) => {
-    console.log(`Row ${rowIndex}: ${JSON.stringify(row.values)}`)
-  })
-}
-async function testReadExcel(filePath: string): Promise<void> {
-  fs.readFile(filePath, async (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-
-    await readExcel(data);
-  });
-}
-async function readFileExcel(file: File): Promise<void> {
-  const buffer = await new Promise<Buffer>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const { result } = event.target as FileReader;
-      if (result instanceof ArrayBuffer) {
-        resolve(Buffer.from(result));
-      } else {
-        reject(new Error('Unexpected result from FileReader'));
-      }
-    };
-    reader.onerror = (error) => {
-      reject(error);
-    };
-    reader.readAsArrayBuffer(file);
-  });
-
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(buffer);
-
-  const worksheet = workbook.getWorksheet(1);
-
-  worksheet.eachRow((row: Row, rowIndex: number) => {
-    console.log(`Row ${rowIndex}: ${JSON.stringify(row.values)}`);
-  });
-}
-
-
-function updateScheduledTasks(tasks: Task[]): void {
-  // 清空当前的定时任务
-
-  // 根据新的任务列表创建定时任务
-
-  // ...
-}
 main();
